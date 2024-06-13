56078f92226879cc9789d739412f552e6b219122
Appolonov1804 <appolonov1804@gmail.com>
2024-06-13 11:24:15 +0600

    corrected controller and READ.me

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -5,56 +5,27 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Client;
-use Spatie\FlareClient\Http\Exceptions\NotFound;
-use Illuminate\Support\Facades\Validator;
-
-/**
- * @OA\Get(
- *     path="/api/clients/show/{phoneNumber}",
- *     summary="Получение информации о клиенте по номеру телефона",
- *     tags={"Clients"},
- *     @OA\Parameter(
- *         name="phoneNumber",
- *         in="path",
- *         required=true,
- *         @OA\Schema(type="string")
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="Информация о клиенте",
- *         @OA\JsonContent(
- *             type="object",
- *             @OA\Property(property="client", type="array", @OA\Items(ref="#/components/schemas/Client")),
- *             @OA\Property(property="status", type="integer")
- *         )
- *     ),
- *     @OA\Response(
- *         response=404,
- *         description="Клиент не найден",
- *         @OA\JsonContent(
- *             type="object",
- *             @OA\Property(property="error", type="string"),
- *             @OA\Property(property="status", type="integer")
- *         )
- *     )
- * )
- */
+use Illuminate\Http\JsonResponse;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ClientController extends Controller
 {
-    public function show(string $phoneNumber) 
+    public function show(string $phoneNumber): JsonResponse
     {
+        try {
+            $client = Client::where('phone_number', $phoneNumber)->firstOrFail();
 
-        $clients = Client::where('phone_number', $phoneNumber)->get();
-        
-        if ($clients->isNotEmpty()) {
             return response()->json([
-                'client' => $clients,
+                'client' => $client,
                 'status' => 200
             ]);
-        } 
-        throw new NotFound();
 
-
+        } catch (ModelNotFoundException $exception) {
+            Log::info($exception->getMessage());
+            return response()->json([
+                'message' => 'Client not found',
+                'status' => 404
+            ], 404);
+        }
     }
 }
